af3e8fdf00e33c15314931ea941a95163d5d176c
SeungMin Shin <sky2197kr@gmail.com>
2023-05-29 15:37:10 +0900

    feat: Add getServerSideProps to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,24 @@
 import Seo from "@/components/Seo";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [movies, setMovies] = useState(null);
-
-  useEffect(() => {
-    fetch('api/movies')
-      .then((res) => res.json())
-      .then((data) => setMovies(data.results));
-  }, []);
+export default function Home({ results }) {
 
   return (
     <>
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div key={movie.id}>
           <h4>{movie.title}</h4>
         </div>))}
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch('http://localhost:3000/api/movies')).json();
+
+  return {
+    props: {
+      results,
+    }
+  }
 }
